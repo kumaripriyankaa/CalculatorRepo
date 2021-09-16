09b086af9d1ef08171f477e25ea7ea496e00f120
PriyankaKumari <priyankak@testvagrant.com>
2021-09-16 17:34:59 +0530

    Added today's code (16th sept 2021) in Git gui and commiting it. Added Calculator, StringUtilTest Classes

---
+++ src/test/java/Calculator.java
@@ -0,0 +1,11 @@
+public class Calculator {
+
+    public int getResultOfAddition(int number1, int number2) {
+        int sum = number1 + number2;
+        return sum;
+    }
+    public int getResultOfMultiplication(int number1, int number2) {
+        int result = number1 * number2;
+        return result;
+    }
+}
--- src/test/java/StringUtilTest.java
+++ src/test/java/StringUtilTest.java
@@ -6,7 +6,28 @@ public class StringUtilTest {
     @Test
         public void shouldGetLengthOfTheString()
     {
-        String testString = "GoodLuck for new journey" ;
-        Assert.assertEquals(testString.length(),24);
+        new Calculator();
+        int length = "GoodLuck for new journey".length();
+
+        Assert.assertEquals(length,24);
     }
+
+    @Test
+        public void shouldAddTwoNumbers()
+
+    {
+        int sum = new Calculator().getResultOfAddition(10,20);
+
+        Assert.assertEquals(sum,30);
+    }
+
+
+    @Test
+    public  void shouldMultiplyTwoNumbers() {
+        int result = new Calculator().getResultOfMultiplication(40,5);
+
+        Assert.assertEquals(result,200);
+    }
+
+
 }
